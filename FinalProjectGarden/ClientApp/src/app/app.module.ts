6e2021d193f2232e7312d5bee34e02a688d80a39
unknown <liamsdonelson@gmail.com>
2022-12-05 15:24:34 -0500

    first

--- FinalProjectGarden/ClientApp/src/app/app.module.ts
+++ FinalProjectGarden/ClientApp/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { SearchedPlantComponent } from './searched-plant/searched-plant.component';
+import { LoginComponent } from './login/login.component';
+import { MyGardenComponent } from './my-garden/my-garden.component';
+import { POTDComponent } from './potd/potd.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    SearchedPlantComponent,
+    LoginComponent,
+    MyGardenComponent,
+    POTDComponent,
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +35,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'SearchedPlant', component: SearchedPlantComponent},
+      { path: 'Login', component: LoginComponent},
+      { path: 'MyGarden', component: MyGardenComponent},
+      { path: 'POTD', component: POTDComponent}
     ])
   ],
   providers: [],
