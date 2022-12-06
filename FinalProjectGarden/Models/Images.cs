6add19b92330e40b43dfaae8828ea283dd510c25
Somber <dj2548@gmail.com>
2022-12-06 10:28:08 -0500

    dock on the bay

---
+++ FinalProjectGarden/Models/Images.cs
@@ -0,0 +1,38 @@
+﻿namespace FinalProjectGarden.Models
+{
+  
+        public class ImageSearch
+        {
+            public int total { get; set; }
+            public int totalHits { get; set; }
+            public Hit[] hits { get; set; }
+        }
+
+        public class Hit
+        {
+            public int id { get; set; }
+            public string pageURL { get; set; }
+            public string type { get; set; }
+            public string tags { get; set; }
+            public string previewURL { get; set; }
+            public int previewWidth { get; set; }
+            public int previewHeight { get; set; }
+            public string webformatURL { get; set; }
+            public int webformatWidth { get; set; }
+            public int webformatHeight { get; set; }
+            public string largeImageURL { get; set; }
+            public int imageWidth { get; set; }
+            public int imageHeight { get; set; }
+            public int imageSize { get; set; }
+            public int views { get; set; }
+            public int downloads { get; set; }
+            public int collections { get; set; }
+            public int likes { get; set; }
+            public int comments { get; set; }
+            public int user_id { get; set; }
+            public string user { get; set; }
+            public string userImageURL { get; set; }
+        }
+
+    }
+
