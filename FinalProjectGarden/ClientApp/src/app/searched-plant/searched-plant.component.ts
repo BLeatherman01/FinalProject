7acc7235b16ad86e3446c9c48d846f65acd1676f
brandon <bleatherman01@gmail.com>
2022-12-07 13:25:35 -0500

    worked on returning image with common name

--- FinalProjectGarden/ClientApp/src/app/searched-plant/searched-plant.component.ts
+++ FinalProjectGarden/ClientApp/src/app/searched-plant/searched-plant.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hit, SearchImage } from '../Services/searched-images';
 import { SearchedImagesService } from '../Services/searched-images.service';
-import { Datum, SearchPlant } from '../Services/searched-plant';
+import { Plant, SearchPlant } from '../Services/searched-plant';
 import { SearchedPlantService } from '../Services/searched-plant.service';
 
 @Component({
@@ -12,12 +12,13 @@ import { SearchedPlantService } from '../Services/searched-plant.service';
 export class SearchedPlantComponent implements OnInit {
   results: SearchPlant = {} as SearchPlant;
   imageResults: SearchImage = {} as SearchImage;
-  imageList: Hit[] = [];
-  plantImage: Hit = {} as Hit;
+  imageList: string [] = [];
+  
   searchPlants: string = '';
-  commonName: string = '';
-
-  list: Datum[] = [];
+  commonName: Plant = {} as Plant
+  
+  name: string = '';
+  list: Plant[] = [];
 
   constructor(
     private plantApi: SearchedPlantService,
@@ -32,17 +33,26 @@ export class SearchedPlantComponent implements OnInit {
       .subscribe((result: SearchPlant) => {
         this.results = result;
         this.list = this.results.data;
-        console.log("hello", this.results.data[2].common_name);
-        this.getImageDetails();
+       this.results.data.forEach((plant: Plant) => {
+           let name =plant.common_name;
+           console.log("check",name)  
+          this.getImageDetails(name);
+          console.log(plant)
+      })
+      
       });
   }
-  getImageDetails(): void {
-    this.ImageApi.getImages(this.searchPlants).subscribe(
+  getImageDetails(name: string): void {
+    this.ImageApi.getImages(name).subscribe(
       (result: SearchImage) => {
-        this.imageResults = result;
-        this.imageList = this.imageResults.hits;
-        console.log(this.imageResults);
-        this.plantImage = this.imageList[2];
+
+        if(result.hits[0]){
+        this.imageList.push(result.hits[0].previewURL);
+        console.log("hits", result.hits[0]);
+        }
+        else{
+          this.imageList.push("/assets/Garden.jpg");
+        }
       }
     );
   }
