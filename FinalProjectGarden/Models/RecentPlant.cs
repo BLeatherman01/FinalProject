4466f68a23ed3e1424c01f36ac8bc06e7f3fd7dd
CryingCoder <112829574+CryingCoder@users.noreply.github.com>
2022-12-07 14:14:37 -0500

    Scaffold Azure SQL DB

---
+++ FinalProjectGarden/Models/RecentPlant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProjectGarden.Models;
+
+public partial class RecentPlant
+{
+    public int Id { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? RecentPlants { get; set; }
+
+    public int? RecentIds { get; set; }
+
+    public virtual User? RecentIdsNavigation { get; set; }
+}
---
+++ FinalProjectGarden/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProjectGarden.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? GoogleId { get; set; }
+
+    public virtual ICollection<MyGarden> MyGardens { get; } = new List<MyGarden>();
+
+    public virtual ICollection<RecentPlant> RecentPlants { get; } = new List<RecentPlant>();
+}
